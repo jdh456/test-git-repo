b4d6f3bf2bf551df34c980d71f1fc123d4be61c8
jdh456 <104139163+jdh456@users.noreply.github.com>
2022-05-12 20:07:13 +0900

    Update led_blink.py

--- led_blink.py
+++ led_blink.py
@@ -1,17 +1,17 @@
-import RPi.GPI0 as GPI0
+import RPi.GPIO as GPIO
 import time
 
 led_pin = 4
 
-GPI0.setwarnings(False)
+GPIO.setwarnings(False)
 
-GPI0.setmode(GPI0.BCM)
+GPIO.setmode(GPIO.BCM)
 
-GPI0.setup(led_pin, GPI0.OUT)
+GPIO.setup(led_pin, GPIO.OUT)
 
 for i in range(10):
-   GPI0.output(led_pin, 1)
+   GPIO.output(led_pin, 1)
    time.sleep(1)
-   GPI0.output(led_pin, 0)
+   GPIO.output(led_pin, 0)
    time.sleep(1)
-GPI0.cleanup()
+GPIO.cleanup()
