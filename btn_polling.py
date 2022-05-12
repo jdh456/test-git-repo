05383a17b6390fa3a3166bb7483677b45ae7099b
jdh456 <104139163+jdh456@users.noreply.github.com>
2022-05-12 20:24:32 +0900

    Update btn_polling.py

--- btn_polling.py
+++ btn_polling.py
@@ -7,7 +7,7 @@ GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO, PUD_DOWN)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while 1:
   if GPIO.input(button_pin) = GPIO.HIGH:
